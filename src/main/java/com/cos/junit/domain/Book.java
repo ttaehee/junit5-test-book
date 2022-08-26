81bc3183d0bd587f273e087a35c220cbdcc15d82
ttaehee <ttaehee1105@gmail.com>
2022-08-26 18:40:36 +0900

    insert 테스트

--- src/main/java/com/cos/junit/domain/Book.java
+++ src/main/java/com/cos/junit/domain/Book.java
@@ -14,21 +14,22 @@ import lombok.NoArgsConstructor;
 @Getter
 @Entity
 public class Book {
-	
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
-	private long id;
-	
-	@Column(length = 50, nullable = false)
-	private String title;
-	
-	@Column(length = 20, nullable = false)
-	private String author;
-	
-	@Builder
+    private Long id;
+
+    @Column(length = 50, nullable = false)
+    private String title;
+    
+    @Column(length = 20, nullable = false)
+    private String author;
+
+    @Builder
     public Book(Long id, String title, String author) {
         this.id = id;
         this.title = title;
         this.author = author;
     }
+
 }
--- src/test/java/com/cos/junit/domain/BookRepositoryTest.java
+++ src/test/java/com/cos/junit/domain/BookRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.cos.junit.domain;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -7,13 +9,28 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 @DataJpaTest
 public class BookRepositoryTest {
 	
-	 @Autowired
-	 private BookRepository bookRepository;
-	 
-	 // 책 등록
-     @Test
-     public void 책등록_test() {
-    	 System.out.print("책등록 test준비");
-     }
+	@Autowired
+	private BookRepository bookRepository;
+	
+	//책등록
+	@Test
+    public void insert_test() {
+		
+        //given (데이터 준비)
+        String title = "junit5";
+        String author = "김태희";
+        Book book = Book.builder()
+                .title(title)
+                .author(author)
+                .build();
+
+        //when(테스트 실행)
+        Book bookPS = bookRepository.save(book);
+
+        //then(검증)
+        assertEquals(title, bookPS.getTitle());
+        assertEquals(author, bookPS.getAuthor());
+        
+    } //트랜잭션 종료(저장된 데이터를 초기화함)
 
 }
