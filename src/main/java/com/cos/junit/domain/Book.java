caac156e6ffa86e05168413a62a646a3f2c979d1
ttaehee <ttaehee1105@gmail.com>
2022-08-26 17:02:50 +0900

    Book entity 생성

--- src/main/java/com/cos/junit/domain/Book.java
+++ src/main/java/com/cos/junit/domain/Book.java
@@ -1,5 +1,34 @@
 package com.cos.junit.domain;
 
-public class Book {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class Book {
+	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+	private long id;
+	
+	@Column(length = 50, nullable = false)
+	private String title;
+	
+	@Column(length = 20, nullable = false)
+	private String author;
+	
+	@Builder
+    public Book(Long id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
 }
