2fda365fe99aef2860ea5630a821fa56179463dc
ttaehee <ttaehee1105@gmail.com>
2022-09-18 14:38:21 +0900

    Controller : getBookOne

--- src/main/java/com/cos/junit/web/BookApiController.java
+++ src/main/java/com/cos/junit/web/BookApiController.java
@@ -9,7 +9,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,7 +47,7 @@ public class BookApiController {
         BookRespDto bookRespDto = bookService.insertBook(bookSaveReqDto);
         
         return new ResponseEntity<>(CommonRespDto.builder().code(1).msg("글 저장 성공").body(bookRespDto).build(),
-                HttpStatus.CREATED);
+                HttpStatus.CREATED);  //201 = insert
     }
     
     //책목록보기
@@ -55,7 +57,17 @@ public class BookApiController {
         BookListRespDto bookListRespDto = bookService.selectAllBook();
         
         return new ResponseEntity<>(CommonRespDto.builder().code(1).msg("글 목록보기 성공").body(bookListRespDto).build(),
-                HttpStatus.OK);
+                HttpStatus.OK);  //200 = ok
+    }
+    
+    //책한건보기
+    @GetMapping("/api/v1/book/{id}")
+    public ResponseEntity<?> getBookOne(@PathVariable Long id) {
+    	
+        BookRespDto bookRespDto = bookService.selectBook(id);
+        
+        return new ResponseEntity<>(CommonRespDto.builder().code(1).msg("글 한건보기 성공").body(bookRespDto).build(),
+                HttpStatus.OK); // 200 = ok
     }
 
 }
