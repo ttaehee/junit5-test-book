b7e67d6f8cd890cae908bd29b2b7c3e752b1ace9
ttaehee <ttaehee1105@gmail.com>
2022-09-30 02:01:16 +0900

    Mail 클래스 완성(다른사람이 구현한다고 가정) 후 MailSenderAdapter 코드만 수정하기 -> 본코드는 수정불필요

--- src/main/java/com/cos/junit/util/MailSenderAdapter.java
+++ src/main/java/com/cos/junit/util/MailSenderAdapter.java
@@ -2,18 +2,18 @@ package com.cos.junit.util;
 
 import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 public class MailSenderAdapter implements MailSender {
-	
-	/*
-	 * private Mail mail;
-	 * 
-	 * public MailSenderAdapter() { this.mail = new Mail(); }
-	 */
+
+	 private Mail mail;
+
+	 public MailSenderAdapter() {
+		 this.mail = new Mail(); 
+	 }	 
 
 	@Override
 	public boolean send() {
-		return true;
+		return mail.sendMail();
 	}
 
 }
